b01b3bf34bfd8e4845b1d6744a7b31eb2f31e504
Heidi <heidi.willden@me.com>
2022-05-04 16:12:55 -0500

    React router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Login from "./component/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
